0055836fc2b41be0fbd9d22415c7ca3ad73f2328
omarinhos <omarmarinosaguilar@gmail.com>
2022-12-04 17:02:19 -0500

    iniciando

---
+++ src/main/java/com/omarinhos/challenge/one/conversor/ChallengeOneConversor.java
@@ -0,0 +1,8 @@
+package com.omarinhos.challenge.one.conversor;
+
+public class ChallengeOneConversor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
